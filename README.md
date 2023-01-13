30c241fc00a433e93071da3423077320ed12f47d
Guilherme Cândido dos Santos <54542736+guilhermecandidosantos@users.noreply.github.com>
2023-01-13 17:51:27 -0300

    desafio finalizado

---
+++ README.md
@@ -0,0 +1,14 @@
+# To-Do List
+
+## Tecnologias utilizadas
+
+- ReactJS
+- Typescript
+- Vite
+
+# Funcionalidades 
+
+- [] Criação de novas tarefas
+- [] Lista de tarefas
+- [] Remoção de tarefa da lista
+- [] Progresso de cada tarefa
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import './global.css';
+import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
+import { FormEvent, useState } from 'react';
+
+export interface ITask{
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const [newTask, setNewTask] = useState("");
+
+  function handleCreateTask(taskTitle: string) {
+    setTasks([...tasks, {
+      id: crypto.randomUUID(),
+      title: taskTitle,
+      isCompleted: false
+    }])
+  }
+
+  function deleteTask(taskId: string) {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function completeTask(taskId: string) {
+    const newTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+            isCompleted: !task.isCompleted
+        }
+      }
+      return task
+    })
+
+    setTasks(newTask);
+  }
+
+  return (
+      <>
+        <Header handleCreateTask={handleCreateTask}/>
+        <TaskList tasks={tasks} onDelete={deleteTask} onCompleted={completeTask}/>
+      </>
+  )
+}
+
+export default App
